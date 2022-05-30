274152f9ce24aac54f4365193021035a8713d017
Simone Omodei <simone.omodei@outlook.com>
2022-05-30 18:43:58 +0200

    Create GUI(Grafic User Interface)

--- rock-scissor-paper/scripts.js
+++ rock-scissor-paper/scripts.js
@@ -1,24 +1,41 @@
+const computerPlayDisplay = document.querySelector("#computer-play");
+const resultPlayDisplay = document.querySelector(".result-container");
+
+const gamesPlayedDisplay = document.querySelector("#games-played");
+const userWinsDisplay = document.querySelector("#user-wins");
+const computerWinsDisplay = document.querySelector("#computer-wins");
+
+const winnerDisplay = document.querySelector(".winner-container");
+
+let gamesPlay = 0;
+
+let playerWins = 0;
+let computerWins = 0;
+
+// Function to generate the play of the computer
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   let computerPlay = "";
+
   switch (randomNumber) {
     case 1:
       computerPlay = "Rock";
-      console.log("Rock");
+      computerPlayDisplay.textContent = "Computer play: Rock";
       break;
     case 2:
       computerPlay = "Scissor";
-      console.log("Scissor");
+      computerPlayDisplay.textContent = "Computer play: Scissor";
       break;
     case 3:
       computerPlay = "Paper";
-      console.log("Paper");
+      computerPlayDisplay.textContent = "Computer play: Paper";
       break;
   }
 
   return computerPlay;
 }
 
+// Function to standardize the input
 function capitalize(string) {
   let str = string.toLowerCase();
   let uppStr = "";
@@ -56,24 +73,41 @@ function playGame(playerSelection, computerSelection) {
     result = `You win! ${playerGame} beats ${computerSelection}`;
   }
 
+  //console.log(result);
+  resultPlayDisplay.innerText = result;
   return result;
 }
 
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
+function game(play) {
+  // Old for to play a fixed number of game
+  //for (let i = 0; i < 5; i++) {}
 
-  for (let i = 0; i < 5; i++) {
-    let result = playGame(prompt("Make your play!"), computerPlay());
-    console.log(result);
-    //if computer wins increment computerWin else increment playerWin
-    if (result.includes("win")) {
-      playerWins++;
-    } else if (result.includes("lose")) {
-      computerWins++;
-    }
+  let result = playGame(play, computerPlay());
+  console.log(result);
+
+  //if computer wins increment computerWin else increment playerWin
+  if (result.includes("win")) {
+    playerWins++;
+  } else if (result.includes("lose")) {
+    computerWins++;
   }
 
+  gamesPlay++;
+
+  gamesPlayedDisplay.textContent = gamesPlay;
+  userWinsDisplay.textContent = playerWins;
+  computerWinsDisplay.textContent = computerWins;
+
+  if (playerWins === 5) {
+    winnerDisplay.textContent = `You win the game ${playerWins} - ${computerWins}`;
+  }
+
+  if (computerWins === 5) {
+    winnerDisplay.textContent = `You lose the game ${playerWins} - ${computerWins}`;
+  }
+
+  // Old winner declaration after a fixed number of games
+  /*
   if (playerWins > computerWins) {
     console.log(`You win the game ${playerWins} - ${computerWins}`);
   } else if (playerWins < computerWins) {
@@ -81,6 +115,14 @@ function game() {
   } else {
     console.log(`Even game ${playerWins} - ${computerWins}`);
   }
+  */
 }
 
-game();
+const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", () => game("Rock"));
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", () => game("Paper"));
+
+const btnScissor = document.querySelector("#btnScissor");
+btnScissor.addEventListener("click", () => game("Scissor"));
